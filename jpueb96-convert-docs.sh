eddd0faf5bc5569fcc878680a2d9697d702c61ed
Jailine Puebla <jpueb96@email.unc.edu>
2016-10-25 21:04:00 +0000

    created md file and edited it

---
+++ jpueb96-convert-docs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Define an Exclamation
+EXCLAMATION="$1"
+
+# Say "Hello to me" 
+echo "Hey, you! $EXCLAMATION"
+
+# Ask for a name
+echo "Who are you?"
+
+# Read in the name
+read NAME
+
+# Ask how their day has been
+echo "How has your day gone?"
+
+# Read in the day
+read DAY
+
+
+# Declare input argument as a variable
+
+# Convert markdown to HTML
+
+# Convert markdown to DOCX
+
+# Convert markdown to ODT
+
+# Convert markdown to PDF
+
+# Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
